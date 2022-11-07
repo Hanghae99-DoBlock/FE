634f3aa035fdd679c22548d1590d6b8433a5d17b
NyeongDev <90minyeong90@gmail.com>
2022-11-08 01:25:14 +0900

    init

---
+++ src/GlobalStyle.js
@@ -0,0 +1,49 @@
+import { createGlobalStyle } from "styled-components";
+import reset from "styled-reset";
+
+const GlobalStyles = createGlobalStyle`
+
+    ${reset}
+
+  * {
+    box-sizing: border-box;
+    margin: 0;
+  }
+
+  body, div, span, h1, h2, h3, h4, h5, h6,
+  p, i, ol, ul, li, form, label, header, nav, 
+  input, textarea, button {
+	margin: 0;
+	padding: 0;
+	border: 0;
+  /* font-family: */
+  }
+  
+  a {
+    color: inherit;
+    text-decoration: none;
+  }
+
+  button {
+    cursor: pointer;
+    letter-spacing: 0.1em;
+  }
+
+  ul, ol, li {
+    list-style: none;
+    padding-left: 0;
+    margin-left: 0;
+  }
+
+  button:focus,
+  button:active,
+  textarea:focus,
+  textarea:active,
+  input:focus,
+  input:active {
+    box-shadow: none;
+    outline: none;      
+  }
+`;
+
+export default GlobalStyles;
