b0ebdb3e2d0e23ae06e9abce6b90879016223840
Nyeong <90minyeong90@gmail.com>
2022-11-11 22:35:30 +0900

    Design: TodoList 컴포넌트 스타일 작업

--- src/common/button/Button.jsx
+++ src/common/button/Button.jsx
@@ -8,8 +8,17 @@ export default Button;
 const StBtn = styled.button`
 	${({ variant }) => {
 		switch (variant) {
-			case "":
-				return css``;
+			// 투두 추가 버튼
+			case "addTodo":
+				return css`
+					width: 100%;
+					height: 49px;
+					background: #7474ff;
+					border-radius: 10px;
+					font-weight: 600;
+					font-size: 14px;
+					color: #ffffff;
+				`;
 			default:
 				break;
 		}
---
+++ src/common/input/Input.jsx
@@ -0,0 +1,25 @@
+import styled, { css } from "styled-components";
+
+const Input = ({ children, ...props }) => {
+	return <StInput {...props}>{children}</StInput>;
+};
+export default Input;
+
+const StInput = styled.input`
+	${({ variant }) => {
+		switch (variant) {
+			// 투두 컨텐트 인풋
+			case "todoInput":
+				return css`
+					height: 48px;
+					padding: 12px 0;
+					caret-color: #7474ff;
+					font-weight: 600;
+					font-size: 19px;
+					color: #131313;
+				`;
+			default:
+				break;
+		}
+	}}
+`;
