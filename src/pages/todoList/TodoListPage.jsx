c84d73b5a19f68d24c100cfa272e34b1e1ee1d2b
Nyeong <90minyeong90@gmail.com>
2022-11-11 22:10:51 +0900

    Feat: TodoListPage에 TodoList 컴포넌트 추가
    투두 리스트 헤더, 투두 리스트, 투두 추가 모달 오픈 버튼으로 쪼갬
    
    Issue: #6

--- src/pages/todoList/TodoListPage.jsx
+++ src/pages/todoList/TodoListPage.jsx
@@ -1,5 +1,25 @@
-const TodoLisPage = () => {
-	return <div>TodoLisPage</div>;
+import { useDispatch } from "react-redux";
+import { updateIsAddTodoModalOpen } from "../../redux/modules/modal/modalSlice";
+import { Box, Svg } from "../../common";
+import { ListingTodos, TodoListHeader } from "../../components";
+
+const TodoListPage = () => {
+	const dispatch = useDispatch();
+
+	const openAddTodoModalHandler = () => {
+		dispatch(updateIsAddTodoModalOpen());
+	};
+
+	return (
+		<Box variant="todoListArea">
+			{/* 투두 리스트 헤더 */}
+			<TodoListHeader />
+			{/* 투두 리스트 */}
+			<ListingTodos />
+			{/* 투두 추가 모달 오픈 버튼 */}
+			<Svg onClick={openAddTodoModalHandler} variant="addTodo" />
+		</Box>
+	);
 };
 
-export default TodoLisPage;
+export default TodoListPage;
