d10aa118665cbd21f698cc85a9110a2cc70544ea
ose1012 <nnmmaz95@gmail.com>
2022-11-17 02:13:01 +0900

    Fix : 병합으로 삭제된 파일 재생성

--- src/common/layout/Layout.jsx
+++ src/common/layout/Layout.jsx
@@ -17,6 +17,8 @@ const StLayout = styled.div`
 					min-width: 320px;
 					max-width: 430px;
 					margin: 0 auto;
+					height: 100%;
+					position: relative;
 				`;
 			default:
 				break;
--- src/redux/config/configStore.js
+++ src/redux/config/configStore.js
@@ -2,12 +2,14 @@ import { configureStore, getDefaultMiddleware } from "@reduxjs/toolkit";
 import join from "../modules/joinSlice";
 import modalSlice from "../modules/modal/modalSlice";
 import todoListSlice from "../modules/todoList/todoListSlice";
+import profileSlice from "../modules/profileSlice";
 
 const store = configureStore({
 	reducer: {
 		join,
 		modalSlice,
 		todoListSlice,
+		profileSlice,
 	},
 	middleware: getDefaultMiddleware({
 		serializableCheck: false,
