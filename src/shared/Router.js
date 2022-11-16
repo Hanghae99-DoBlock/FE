ced9ebe5c3698b8369e94bdd8d04777066d14a47
ose1012 <nnmmaz95@gmail.com>
2022-11-17 01:29:07 +0900

    Update : 프로필 수정 페이지, 비밀번호 변경 페이지 파일 이름 변경

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,5 +1,4 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
-<<<<<<< HEAD
 import {
 	ProfilePage,
 	SignInPage,
@@ -8,9 +7,6 @@ import {
 	ProfileEditPage,
 	PasswordChangePage,
 } from "../pages";
-=======
-import { ProfilePage, SignInPage, SignUpPage, TodoListPage } from "../pages";
->>>>>>> ddfcd49c2bf9e3a3bd984ba6f17015a11502e834
 
 const Router = () => {
 	return (
@@ -19,16 +15,12 @@ const Router = () => {
 				<Routes>
 					<Route path="/signin" element={<SignInPage />} />
 					<Route path="/signup" element={<SignUpPage />} />
-<<<<<<< HEAD
 					<Route path="/profile/:id" element={<ProfilePage />} />
 					<Route path="/profile/edit/:id" element={<ProfileEditPage />} />
 					<Route
 						path="/profile/edit/password"
 						element={<PasswordChangePage />}
 					/>
-=======
-					<Route path="/:id" element={<ProfilePage />} />
->>>>>>> ddfcd49c2bf9e3a3bd984ba6f17015a11502e834
 					<Route path="/" element={<TodoListPage />} />
 				</Routes>
 			</BrowserRouter>
