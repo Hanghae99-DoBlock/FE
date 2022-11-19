d9e9cc3adc1e9ec13b5984c47fbb292403fef427
Nyeong <90minyeong90@gmail.com>
2022-11-19 14:38:56 +0900

    Fix: 충돌 해결

--- src/pages/todoList/TodoListPage.jsx
+++ src/pages/todoList/TodoListPage.jsx
@@ -1,11 +1,6 @@
 import { useDispatch } from "react-redux";
-import { Box, Flex, Svg } from "../../common";
-import {
-	TodoListCalendar,
-	ListingTodos,
-	TodoListHeader,
-	NavBelow,
-} from "../../components";
+import { Flex, Svg } from "../../common";
+import { TodoListCalendar, NavBelow } from "../../components";
 import { updateIsAddTodoModalOpen } from "../../redux/modules/modal/modalSlice";
 
 const TodoListPage = () => {
@@ -17,8 +12,12 @@ const TodoListPage = () => {
 
 	return (
 		<>
-			<Flex dir="column" ht="100%">
+			<Flex dir="column" ht="100vh" bg="#F9F9F9">
 				<TodoListCalendar />
+				{/* 투두 추가 모달 오픈 버튼 */}
+				<Flex wd="100%" position="sticky" bottom="70px" jc="flex-end" pd="10px">
+					<Svg onClick={openAddTodoModalHandler} variant="addTodo" />
+				</Flex>
 			</Flex>
 			<NavBelow />
 		</>
