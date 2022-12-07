7207935d61dbdb6daa4fc408448fa439c8cdd1bf
Nyeong <90minyeong90@gmail.com>
2022-12-07 21:20:18 +0900

    Fix: 새로고침 시 시간이 새로고침 되어 리프레시 토큰을 제때 갱신하지 못하던 현상 해결

--- src/components/join/RefreshToken.jsx
+++ src/components/join/RefreshToken.jsx
@@ -1,45 +1,61 @@
 import axios from "axios";
-import { useState } from "react";
+import dayjs from "dayjs";
+import jwtDecode from "jwt-decode";
+import { useEffect, useState } from "react";
 import { Outlet } from "react-router-dom";
 import { ModalAlertExpirationToken } from "../../components";
 
 const RefreshToken = () => {
-	const [sAlertExpirationTokenModalOpen, setIsAlertExpirationTokenModalOpen] =
+	const token = localStorage.getItem("accessToken");
+	const tokenExpirationTime = jwtDecode(token).exp;
+	const timeBeforeTokenExpiration = dayjs
+		.unix(tokenExpirationTime)
+		.subtract(10, "minute").$d;
+
+	const [timeNow, setTimeNow] = useState(new Date());
+	const [isAlertExpirationTokenModalOpen, setIsAlertExpirationTokenModalOpen] =
 		useState(false);
 
-	const requestRefreshToken = setInterval(() => {
-		const accessToken = localStorage.getItem("accessToken");
-		const refreshToken = localStorage.getItem("refreshToken");
-		axios
-			.post(
-				`${process.env.REACT_APP_SERVER_URL}/api/members/reissue`,
-				{
-					withCredentials: true,
-				},
-				{
-					headers: {
-						Authorization: accessToken,
-						RefreshToken: refreshToken,
+	useEffect(() => {
+		const setTimeFunc = setInterval(() => {
+			setTimeNow(new Date());
+		}, 60000);
+	}, [new Date()]);
+
+	useEffect(() => {
+		if (timeNow === timeBeforeTokenExpiration) {
+			const accessToken = localStorage.getItem("accessToken");
+			const refreshToken = localStorage.getItem("refreshToken");
+			axios
+				.post(
+					`${process.env.REACT_APP_SERVER_URL}/api/members/reissue`,
+					{
+						withCredentials: true,
 					},
-				},
-			)
-			.then(response => {
-				const accessToken = response.headers.authorization;
-				const refreshToken = response.headers.refreshtoken;
-				window.localStorage.setItem("accessToken", accessToken);
-				window.localStorage.setItem("refreshToken", refreshToken);
-			})
-			.catch(error => {
-				clearInterval(requestRefreshToken);
-				setIsAlertExpirationTokenModalOpen(true);
-				localStorage.removeItem("accessToken");
-				localStorage.removeItem("refreshToken");
-			});
-	}, 600000);
+					{
+						headers: {
+							Authorization: accessToken,
+							RefreshToken: refreshToken,
+						},
+					},
+				)
+				.then(response => {
+					const accessToken = response.headers.authorization;
+					const refreshToken = response.headers.refreshtoken;
+					window.localStorage.setItem("accessToken", accessToken);
+					window.localStorage.setItem("refreshToken", refreshToken);
+				})
+				.catch(error => {
+					setIsAlertExpirationTokenModalOpen(true);
+					localStorage.removeItem("accessToken");
+					localStorage.removeItem("refreshToken");
+				});
+		}
+	}, [timeBeforeTokenExpiration, timeNow]);
 
 	return (
 		<>
-			{sAlertExpirationTokenModalOpen ? (
+			{isAlertExpirationTokenModalOpen ? (
 				<ModalAlertExpirationToken
 					setIsAlertExpirationTokenModalOpen={
 						setIsAlertExpirationTokenModalOpen
