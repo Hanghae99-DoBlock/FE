d7bb6fcb8a734707b9f1055a410ae5c6cb75d8eb
Yeonwoo Ga <vjk4874@naver.com>
2022-12-08 01:26:59 +0900

    Fix : 민영님 요청으로 라우터 변경

--- src/shared/Router.js
+++ src/shared/Router.js
@@ -1,4 +1,5 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
+import { RefreshToken } from "../components";
 import {
 	ProfilePage,
 	SignInPage,
@@ -16,8 +17,8 @@ import {
 	InterestTagsPage,
 	MyBadgesPage,
 	BadgeSetiingPage,
+	ReactionListPage,
 } from "../pages";
-import EditFeedPage from "../pages/feed/EditFeedPage";
 import EmailSignIn from "../pages/join/EmailSignIn";
 import GoogleLogin from "../pages/join/GoogleLogin";
 import KakaoLogin from "../pages/join/KakaoLogin";
@@ -36,133 +37,123 @@ const Router = () => {
 					<Route path="/api/members/login/kakao" element={<KakaoLogin />} />
 					<Route path="/api/members/login/naver" element={<NaverLogin />} />
 					<Route path="/api/members/login/google" element={<GoogleLogin />} />
-					<Route
-						path="/profile/:id"
-						element={
-							<PrivateRoute>
-								<ProfilePage />
-							</PrivateRoute>
-						}
-					/>
-					<Route
-						path="/profile/edit"
-						element={
-							<PrivateRoute>
-								<ProfileEditPage />
-							</PrivateRoute>
-						}
-					/>
-					<Route
-						path="/profile/edit/password"
-						element={<PasswordChangePage />}
-					/>
-					<Route
-						path="/profile/:id/following"
-						element={
-							<PrivateRoute>
-								<FollowingPage />
-							</PrivateRoute>
-						}
-					/>
-					<Route
-						path="/profile/:id/follower"
-						element={
-							<PrivateRoute>
-								<FollowerPage />
-							</PrivateRoute>
-						}
-					/>
-					<Route
-						path="/profile/:id/badges"
-						element={
-							<PrivateRoute>
-								<MyBadgesPage />
-							</PrivateRoute>
-						}
-					/>
-					<Route
-						path="/profile/:id/badgeSetting"
-						element={
-							<PrivateRoute>
-								<BadgeSetiingPage />
-							</PrivateRoute>
-						}
-					/>
-					<Route
-						path="/profile/:id/following"
-						element={
-							<PrivateRoute>
-								<FollowingPage />
-							</PrivateRoute>
-						}
-					/>
-					<Route
-						path="/profile/:id/follower"
-						element={
-							<PrivateRoute>
-								<FollowerPage />
-							</PrivateRoute>
-						}
-					/>
-					<Route
-						path="/profile/edit/interest"
-						element={
-							<PrivateRoute>
-								<InterestTagsPage />
-							</PrivateRoute>
-						}
-					/>
-					<Route
-						path="/todolist"
-						element={
-							<PrivateRoute>
-								<TodoListPage />
-							</PrivateRoute>
-						}
-					/>
-					<Route
-						path="/feed"
-						element={
-							<PrivateRoute>
-								<FeedPage />
-							</PrivateRoute>
-						}
-					>
-						<Route path="following" element={<FollowingFeedListPage />} />
-						<Route path="recommended" element={<RecommendedFeedListPage />} />
+					<Route path="/*" element={<RefreshToken />}>
+						<Route
+							path="profile/:id"
+							element={
+								<PrivateRoute>
+									<ProfilePage />
+								</PrivateRoute>
+							}
+						/>
+						<Route
+							path="profile/edit"
+							element={
+								<PrivateRoute>
+									<ProfileEditPage />
+								</PrivateRoute>
+							}
+						/>
+						<Route
+							path="profile/edit/password"
+							element={
+								<PrivateRoute>
+									<PasswordChangePage />
+								</PrivateRoute>
+							}
+						/>
+						<Route
+							path="profile/:id/badges"
+							element={
+								<PrivateRoute>
+									<MyBadgesPage />
+								</PrivateRoute>
+							}
+						/>
+						<Route
+							path="profile/:id/badgeSetting"
+							element={
+								<PrivateRoute>
+									<BadgeSetiingPage />
+								</PrivateRoute>
+							}
+						/>
+						<Route
+							path="profile/:id/following"
+							element={
+								<PrivateRoute>
+									<FollowingPage />
+								</PrivateRoute>
+							}
+						/>
+						<Route
+							path="profile/:id/follower"
+							element={
+								<PrivateRoute>
+									<FollowerPage />
+								</PrivateRoute>
+							}
+						/>
+						<Route
+							path="profile/edit/interest"
+							element={
+								<PrivateRoute>
+									<InterestTagsPage />
+								</PrivateRoute>
+							}
+						/>
+						<Route
+							path="todolist"
+							element={
+								<PrivateRoute>
+									<TodoListPage />
+								</PrivateRoute>
+							}
+						/>
+						<Route
+							path="feed"
+							element={
+								<PrivateRoute>
+									<FeedPage />
+								</PrivateRoute>
+							}
+						>
+							<Route path="following" element={<FollowingFeedListPage />} />
+							<Route path="recommended" element={<RecommendedFeedListPage />} />
+						</Route>
+						<Route
+							path="addFeed"
+							element={
+								<PrivateRoute>
+									<AddFeedPage />
+								</PrivateRoute>
+							}
+						/>
+						<Route
+							path="feed/:id"
+							element={
+								<PrivateRoute>
+									<DetailFeedPage />
+								</PrivateRoute>
+							}
+						/>
+						<Route
+							path="feed/reactionList"
+							element={
+								<PrivateRoute>
+									<ReactionListPage />
+								</PrivateRoute>
+							}
+						/>
+						<Route
+							path="search"
+							element={
+								<PrivateRoute>
+									<SearchPage />
+								</PrivateRoute>
+							}
+						/>
 					</Route>
-					<Route
-						path="/addFeed"
-						element={
-							<PrivateRoute>
-								<AddFeedPage />
-							</PrivateRoute>
-						}
-					/>
-					<Route
-						path="/feed/:id"
-						element={
-							<PrivateRoute>
-								<DetailFeedPage />
-							</PrivateRoute>
-						}
-					/>
-					<Route
-						path="/feedEdit/:id"
-						element={
-							<PrivateRoute>
-								<EditFeedPage />
-							</PrivateRoute>
-						}
-					/>
-					<Route
-						path="/search"
-						element={
-							<PrivateRoute>
-								<SearchPage />
-							</PrivateRoute>
-						}
-					/>
 				</Routes>
 			</BrowserRouter>
 		</>
