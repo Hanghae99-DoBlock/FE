0139a5996cad0da01740991ae8f8fcdaa2a9504b
ose1012 <nnmmaz95@gmail.com>
2022-12-13 13:43:13 +0900

    Fix : 대표 뱃지 설정 안보이는 이슈 해결

--- src/pages/profile/MyBadgesPage.jsx
+++ src/pages/profile/MyBadgesPage.jsx
@@ -48,78 +48,76 @@ const MyBadgesPage = () => {
 					</Flex>
 					<Flex wd="135px" ht="42px" jc="center" mg="0 17px 0 0"></Flex>
 				</Flex>
-				{decodeToken.memberId === representativeBadge.memberId ? (
-					representativeBadge.badgeImage !== null ? (
-						<>
-							<Flex fw="600" fs="18" mg="40px auto 15px auto">
-								나의 대표 뱃지
-							</Flex>
-							<Flex fw="300" fs="14">
-								뱃지는 피드에서 닉네임과 함께 노출됩니다.
-							</Flex>
-							<Flex mg="20px 0 0 0">
-								<Image src={representativeBadge.badgeImage}></Image>
-							</Flex>
-							<Flex
-								wd="120px"
-								ht="37px"
-								bg="#FFF4ED"
-								radius="8px"
-								color="#FF8737"
-								fw="600"
-								fs="14"
-							>
-								{representativeBadge.badgeName}
-							</Flex>
-							<Flex
-								pd="10px 10px 10px 12px"
-								wd="120px"
-								ht="37px"
-								bg="#131313"
-								radius="5px"
-								color="#fff"
-								fw="600"
-								fs="12"
-								mg="30px 0 0 0"
-								cursor="pointer"
-								onClick={() => {
-									navigate(`/profile/${id}/badgeSetting/`);
-								}}
-							>
-								대표 뱃지 설정
-								<Svg variant="badgeRightArrow"></Svg>
-							</Flex>
-						</>
-					) : (
-						<>
-							<Flex fw="600" fs="18" mg="40px auto 15px auto">
-								대표 뱃지가 없습니다.
-							</Flex>
-							<Flex fw="300" fs="14">
-								뱃지는 피드에서 닉네임과 함께 노출됩니다.
-							</Flex>
-							<Svg variant="profileBlock"></Svg>
-							<Flex
-								pd="10px 10px 10px 12px"
-								wd="120px"
-								ht="37px"
-								bg="#131313"
-								radius="5px"
-								color="#fff"
-								fw="600"
-								fs="12"
-								mg="30px 0 0 0"
-								cursor="pointer"
-								onClick={() => {
-									navigate(`/profile/${id}/badgeSetting/`);
-								}}
-							>
-								대표 뱃지 설정
-								<Svg variant="badgeRightArrow"></Svg>
-							</Flex>
-						</>
-					)
-				) : null}
+				{representativeBadge.badgeImage !== null ? (
+					<>
+						<Flex fw="600" fs="18" mg="40px auto 15px auto">
+							나의 대표 뱃지
+						</Flex>
+						<Flex fw="300" fs="14">
+							뱃지는 피드에서 닉네임과 함께 노출됩니다.
+						</Flex>
+						<Flex mg="20px 0 0 0">
+							<Image src={representativeBadge.badgeImage}></Image>
+						</Flex>
+						<Flex
+							wd="120px"
+							ht="37px"
+							bg="#FFF4ED"
+							radius="8px"
+							color="#FF8737"
+							fw="600"
+							fs="14"
+						>
+							{representativeBadge.badgeName}
+						</Flex>
+						<Flex
+							pd="10px 10px 10px 12px"
+							wd="120px"
+							ht="37px"
+							bg="#131313"
+							radius="5px"
+							color="#fff"
+							fw="600"
+							fs="12"
+							mg="30px 0 0 0"
+							cursor="pointer"
+							onClick={() => {
+								navigate(`/profile/${id}/badgeSetting/`);
+							}}
+						>
+							대표 뱃지 설정
+							<Svg variant="badgeRightArrow"></Svg>
+						</Flex>
+					</>
+				) : (
+					<>
+						<Flex fw="600" fs="18" mg="40px auto 15px auto">
+							대표 뱃지가 없습니다.
+						</Flex>
+						<Flex fw="300" fs="14">
+							뱃지는 피드에서 닉네임과 함께 노출됩니다.
+						</Flex>
+						<Svg variant="profileBlock"></Svg>
+						<Flex
+							pd="10px 10px 10px 12px"
+							wd="120px"
+							ht="37px"
+							bg="#131313"
+							radius="5px"
+							color="#fff"
+							fw="600"
+							fs="12"
+							mg="30px 0 0 0"
+							cursor="pointer"
+							onClick={() => {
+								navigate(`/profile/${id}/badgeSetting/`);
+							}}
+						>
+							대표 뱃지 설정
+							<Svg variant="badgeRightArrow"></Svg>
+						</Flex>
+					</>
+				)}
 				<Flex wd="375px" ht="4px" bg="#F8F8F8" mg="40px 0 20px 0"></Flex>
 			</Flex>
 			<Flex mw="375px" mxw="375px" wrap="wrap" mg="0 auto 100px auto">
